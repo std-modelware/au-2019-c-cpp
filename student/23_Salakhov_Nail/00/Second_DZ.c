0ea04002b3b20ba1b55a9bc4bf0e95a1173d6a60
nbdpqn <55291932+nbdpqn@users.noreply.github.com>
2019-09-28 12:40:44 +0300

    Update Second_DZ.c

--- student/23_Salakhov_Nail/00/Second_DZ.c
+++ student/23_Salakhov_Nail/00/Second_DZ.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 main() {
-	int a, b, c;
+	int a, b, c, x;
 	scanf_s("%d %d", &a, &b);
 	c = a % b;
-	printf("%d mod %d = %d", a, b, (c > 0) ? c : -1 * c);
-	/*
-	  27 mod 4 is 3
-	 -15 mod 4 is -3
-	 113 mod -3 is 2
-	 -15 mod -7 is -1
-	*/
+	x = (b > 0) ? b : -1 * b;
+	printf("%d mod %d = %d", a, b, (c > 0) ? c : c + x);
 }
