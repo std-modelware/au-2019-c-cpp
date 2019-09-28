f3327b45a69eb0d98257e111deecdfa04f0caf73
Vazgen25 <aivazjan.vazgen@gmail.com>
2019-09-28 11:26:55 +0300

    Домашее задание №2

---
+++ student/01-Aivazjan-Vazgen/21_09/21_09/21_09.cpp
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//ввод делимого
+int read_dividend() {
+	int a;
+	printf("Enter the dividend: ");
+	scanf("%d", &a);
+	return a;
+}
+
+//ввод делителя
+int read_divider() {
+	int a;
+	printf("Enter the divider: ");
+	scanf("%d", &a);
+	return a;
+}
+
+// деление с остатком
+// a-делимое  b-делитель
+int mod(int a, int b) {
+	int c = a % b;
+	if (c < 0) if (b > 0) c += b; else c -= b;
+	return (c);
+}
+
+// целая часть от деления
+// a-делимое  b-делитель
+/*int div(int a, int b) {
+	return (a / b);
+}
+*/
+//вывод результата
+void write_result(int a, int b) {
+	printf("Whole part: %d\n", a);
+	printf("Remainder: %d\n", b);
+	printf("\n");
+}
+
+void work_cycle() {
+	int a; int b;
+	while (true)
+	{
+		a = read_dividend();
+		b = read_divider();
+		if (b != 0) write_result(a / b, mod(a,b));
+		else
+		{
+			printf("Error \"Zero can't be a divisor\"");
+			break;
+		}
+	}
+}
+
+
+int main()
+{
+	work_cycle();
+	return 0;
+}
