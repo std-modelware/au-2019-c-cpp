5179a7e2dc913fd34d50608b2edc812385799ae1
mrgyber <39025153+mrgyber@users.noreply.github.com>
2019-10-02 19:49:35 +0300

    Update main.c

--- student/15_Kudryavtsev_Nikita/1/main.c
+++ student/15_Kudryavtsev_Nikita/1/main.c
@@ -4,65 +4,84 @@
 void method_one(long int a, long int b) {
     printf("\n======= METHOD 1 =======");
     clock_t start = clock();
-    
+
     long int result = a % b;
-    
+
     clock_t end = clock();
     double seconds = (double)(end - start) / CLOCKS_PER_SEC;
     printf("\nResult: %ld\n", result);
     printf("Time: %f seconds\n", seconds);
 }
 
+
+long int mod(long int a, long int b) {
+    if (a == b) {
+        return 0;
+    }
+    else if (a > b) {
+        while (a >= b) {
+            a -= b;
+        }
+    }
+    else {
+        while (a <= b) {
+            a += b;
+        }
+    }
+    return a;
+}
+
+
 void method_two(long int a, long int b) {
     printf("\n======= METHOD 2 =======");
     clock_t start = clock();
+    long int ost = 0;
+    long int mod_;
 
     if (a < 0 && b > 0) {
-        b = -b;
-        while (a <= b) {
-            a -= b;
-        }
+        mod_ = mod(-a, b);
+        ost = b - mod_;
     }
     else if (a > 0 && b < 0) {
         b = -b;
-        while (a >= b) {
-            a -= b;
-        }
+        ost = mod(a, b);
     }
     else if (a > 0 && b > 0) {
-        while (a >= b) {
-            a -= b;
-        }
+        ost = mod(a, b);
     }
     else if (a < 0 && b < 0) {
-        while (a <= b) {
-            a -= b;
-        }
+        b = -b;
+        mod_ = mod(-a, b);
+        ost = b - mod_;
     }
-    long int result = a;
 
     clock_t end = clock();
     double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("\nResult: %ld\n", result);
+    printf("\nResult: %ld\n", ost);
     printf("Time: %f seconds\n", seconds);
 }
 
 void test() {
     long int a;
     long int b;
+    char percent = 37;
 
+    printf("\n\n\n27 %c 4", percent);
     a = 27;
     b = 4;
     method_one(a, b);
     method_two(a, b);
+    printf("\n\n\n-15 %c 4", percent);
     a = -15;
     b = 4;
     method_one(a, b);
     method_two(a, b);
+    printf("\n\n\n113 %c -3", percent);
     a = 113;
     b = -3;
     method_one(a, b);
     method_two(a, b);
+    printf("\n\n\n-15 %c -7", percent);
     a = -15;
     b = -7;
     method_one(a, b);
