2d5897f3c03ec63828bc4a58f13f44aae802eb54
vladik7063 <40918996+vladik7063@users.noreply.github.com>
2019-10-12 09:08:23 +0300

    update

---
+++ student/30_Yaroslavtsev_Vlad/homework/Dz4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+typedef struct Node {
+	int value;
+	struct Node* next;
+} Node;
+
+void push(Node** head, int data) {
+	Node* tmp = (Node*)malloc(sizeof(Node));
+	tmp->value = data;
+	tmp->next = *head;
+	*head = tmp;
+}
+
+void passage(Node* start) {
+	while (start->next) {
+		printf("%d  ", start->value);
+		start = start->next;
+	}
+	printf("%d", start->value);
+	printf("\n");
+}
+
+int main() {
+	Node* head = NULL;
+	for (int i = 1; i <= 10; i++) {
+		push(&head, i);
+	}
+	passage(head);
+	return 0;
+}
+
+
