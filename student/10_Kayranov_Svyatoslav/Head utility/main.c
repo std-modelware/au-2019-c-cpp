c63541e180e12f6172b09a5d9c0d07eec7e00c3e
kairoslav <55292388+kairoslav@users.noreply.github.com>
2019-11-17 01:20:21 +0300

    head 0.2

--- student/10_Kayranov_Svyatoslav/Head utility/main.c
+++ student/10_Kayranov_Svyatoslav/Head utility/main.c
@@ -37,26 +37,23 @@ void checkArgs() {
                 printf("%s'%s'%s\n", "head: cannot open ", fileName, " for reading: No such file or directory");
                 exit(EXIT_FAILURE);
             }
-            else {
-                printf("Все окей!1\n");
-            }
         }
     }
 }
 
 void nOption() {
     if (!(strncmp(option, "-n", 2))) {
-        printf("Все окей!2\n");
         char s[200];
         char c;
         FILE* file;
         int count = 0;
         file = fopen(fileName, "r");
-        printf("Все окей!3\n");
+        printf("Все окей!\n");
         for (count = 0; count < digits; count++) {
+            printf("(%d) ", count+1);
             while(1) {
                 c = fgetc(file);
-                if (c == '\n') break;
+                if ((c == '\n') || (c == EOF))  break;
                 printf("%c", c);
             }
             printf("\n");
