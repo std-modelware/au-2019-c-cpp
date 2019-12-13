b15507676e7c4ffcad048684c5f4353415fcb590
kairoslav <svt210@yandex.ru>
2019-12-13 22:11:59 +0300

    Binary search tree

--- student/10_Kayranov_Svyatoslav/07/binary_tree.c
+++ student/10_Kayranov_Svyatoslav/07/binary_tree.c
@@ -1,22 +1,16 @@
 #include "binary_tree.h"
 
-typedef struct element {
-	int key;
-	int value;
-	struct element* left;
-	struct element* right;
-} element;
 element* kernel;
-element* temp;
 element* pointer;
 
 void create_root(int key, int value) {
-	element* kernel = malloc(sizeof(element));
+	element* kernel =(element*)malloc(sizeof(element));
 	kernel->key = key;
 	kernel->value = value;
 	kernel->left = NULL;
 	kernel->right = NULL;
 	pointer = kernel;
+	kernel = pointer;
 }
 
 
@@ -26,35 +20,167 @@ int tryFind(int key); // return 0 - поиск не удачен, 1 - удачи
 */
 
 //int find(int key); // return value
+
+int count = 0;
+
 int add(int key, int value) {
-	kernel = pointer;
-	element* newElement = malloc(sizeof(element));
+	if (count < 1) {
+		kernel = pointer;
+		count++;
+	}
+
+	element* newElement = (element*)malloc(sizeof(element));
 	newElement->key = key;
 	newElement->value = value;
 	newElement->left = NULL;
 	newElement->right = NULL;
 	if ((pointer != NULL) && (key == pointer->key)) {
-		printf("%s%d%s", "The element '",key,"' already exist.\n" );
+		printf("%s%d%s", "The element '", key, "' already exist.\n");
+		pointer = kernel;
+		kernel = pointer;
 		return(1);
 	}
+	//left
 	if ((pointer != NULL) && (pointer->left != NULL) && (newElement->key < pointer->key)) {
 		pointer = pointer->left;
 		add(key, value);
+		return(0);
 	}
 	if ((pointer->left == NULL) && (newElement->key < pointer->key)) {
 		pointer->left = newElement;
 		pointer = kernel;
-		printf("%s%d%s", "The '", key, "' go es left.\n");
+		printf("%s%d%s", "The '", key, "' goes left.\n");
+		kernel = pointer;
+		return(0);
 	}
-
-	if ((pointer != NULL) && (pointer->right != NULL) &&  (newElement->key > pointer->key)) {
+	//right
+	if ((pointer != NULL) && (pointer->right != NULL) && (newElement->key > pointer->key)) {
 		pointer = pointer->right;
 		add(key, value);
+		return(0);
 	}
 	if ((pointer->right == NULL) && (newElement->key > pointer->key)) {
 		pointer->right = newElement;
 		pointer = kernel;
 		printf("%s%d%s", "The '", key, "' goes right.\n");
+		return(0);
+	}
+}
+
+int find(int key) {
+	if (count < 1) {
+		kernel = pointer;
+		count++;
+	}
+
+	element* newElement = (element*)malloc(sizeof(element));
+	newElement->key = key;
+	newElement->value = NULL;
+	newElement->left = NULL;
+	newElement->right = NULL;
+	if ((pointer != NULL) && (key == pointer->key)) {
+		printf("%s%d%s%d%s", "The element '", key, "' already exist, value = ", pointer->value, ".\n");
+		pointer = kernel;
+		kernel = pointer;
+		return(1);
+	}
+	//left
+	if ((pointer != NULL) && (pointer->left != NULL) && (newElement->key < pointer->key)) {
+		pointer = pointer->left;
+		find(key);
+		return(0);
+	}
+	if ((pointer->left == NULL) && (newElement->key < pointer->key)) {
+		pointer = kernel;
+		printf("%s%d%s", "The '", key, "' didnt find.\n");
+		kernel = pointer;
+		return(0);
+	}
+	//right
+	if ((pointer != NULL) && (pointer->right != NULL) && (newElement->key > pointer->key)) {
+		pointer = pointer->right;
+		find(key);
+		return(0);
+	}
+	if ((pointer->right == NULL) && (newElement->key > pointer->key)) {
+		pointer = kernel;
+		printf("%s%d%s", "The '", key, "' didnt find.\n");
+		return(0);
+	}
+}
+
+int addElem(element* newElement) {
+	if (count < 1) {
+		kernel = pointer;
+		count++;
+	}
+
+	if ((pointer != NULL) && (newElement->key == pointer->key)) {
+		pointer = kernel;
+		kernel = pointer;
+		return(1);
+	}
+	//left
+	if ((pointer != NULL) && (pointer->left != NULL) && (newElement->key < pointer->key)) {
+		pointer = pointer->left;
+		addElem(newElement);
+		return(0);
+	}
+	if ((pointer->left == NULL) && (newElement->key < pointer->key)) {
+		pointer->left = newElement;
+		pointer = kernel;
+		kernel = pointer;
+		return(0);
+	}
+	//right
+	if ((pointer != NULL) && (pointer->right != NULL) && (newElement->key > pointer->key)) {
+		pointer = pointer->right;
+		addElem(newElement);
+		return(0);
+	}
+	if ((pointer->right == NULL) && (newElement->key > pointer->key)) {
+		pointer->right = newElement;
+		pointer = kernel;
+		return(0);
+	}
+}
+
+int removeElem(int key) {
+	if (count < 1) {
+		kernel = pointer;
+		count++;
+	}
+
+	if ((pointer != NULL) && (key == pointer->key)) {
+		printf("%s%d%s", "The element '", key, "' is going to dissapear, pip... ploops... DONE.\n");
+
+		element* right = pointer->right;
+		*pointer = *pointer->left;
+		addElem(right);
+		return(1);
+	}
+	//left
+	if ((pointer != NULL) && (pointer->left != NULL) && (key < pointer->key)) {
+		pointer = pointer->left;
+		removeElem(key);
+		return(0);
+	}
+	if ((pointer->left == NULL) && (key < pointer->key)) {
+		pointer = kernel;
+		printf("%s%d%s", "The '", key, "' didnt find for deleting.\n");
+		kernel = pointer;
+		return(0);
+	}
+	//right
+	if ((pointer != NULL) && (pointer->right != NULL) && (key > pointer->key)) {
+		pointer = pointer->right;
+		removeElem(key);
+		return(0);
+	}
+	if ((pointer->right == NULL) && (key > pointer->key)) {
+		pointer = kernel;
+		printf("%s%d%s", "The '", key, "' didnt find for deleting.\n");
+		return(0);
 	}
 }
 
